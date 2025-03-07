9b507f72740c63fe3341b1e01a02481e21eef809
jaywcjlove <398188662@qq.com>
2025-03-07 13:02:32 +0800

    feat: update PurchasePopupButton

--- Sources/StoreKitHelper/Views/PurchasePopupButton.swift
+++ Sources/StoreKitHelper/Views/PurchasePopupButton.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
-public struct PurchasePopupButton: View {
+public struct PurchasePopupButton<LabelView: View>: View {
     @EnvironmentObject public var store: StoreContext
-    public init() {}
+    var label: (() -> LabelView)?
+    public init(label: (() -> LabelView)? = nil) {
+        self.label = label
+    }
     public var body: some View {
         Button(action: {
             store.isShowingPurchasePopup.toggle()
         }) {
-            Image(systemName: "cart")
+            if let label {
+                label()
+            } else {
+                Image(systemName: "cart")
+            }
         }
     }
 }
 
+public extension PurchasePopupButton where LabelView == EmptyView {
+    init() {
+        self.init(label: nil)
+    }
+}
